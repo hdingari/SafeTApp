2fbd16a33a4fdb6e9b5af1a835cb750f9cee4db7
Hrushikesh Dingari <hdingari@hotmail.com>
2022-01-17 15:20:33 -0500

    Initial Commit

---
+++ SafeTApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SafeTApp
+//
+//  Created by Hrushikesh Dingari on 1/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SafeTApp/SafeTAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeTAppApp.swift
+//  SafeTApp
+//
+//  Created by Hrushikesh Dingari on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct SafeTAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
